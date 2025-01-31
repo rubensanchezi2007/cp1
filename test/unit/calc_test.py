79c98fdd1879075c550290b95f50622279dc4e99
Ruben Sanchez <ruben.sanchezi2007@gmail.com>
2025-01-31 16:46:39 +0100

    CP1_B Reto3

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -39,6 +39,10 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
         self.assertRaises(TypeError, self.calc.divide, "2", "2")
+        
+    def test_divide_method_fails_with_div_zero(self):
+        self.assertRaises(TypeError, self.calc.divide, 2, 0)
+        
 
     def test_multiply_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.multiply(2, 2))
